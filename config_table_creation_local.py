86b99905f5af4e87c1c4e43b6d2e74326cfbe389
RaviralaRajashree <rajashreeravirala@gmail.com>
2024-01-05 15:24:32 +0530

    removed commented code

--- config_table_creation_local.py
+++ config_table_creation_local.py
@@ -29,62 +29,18 @@ def create_table(table_name,cursor_obj):
     table_exists = cursor_obj.fetchone()[0]
     if table_exists:
         print(f"The '{table_name}' already exists.")
-        # If table exists truncate the table
-        #truncate_query=f"TRUNCATE TABLE {table_name}"
-        #print(f"The '{table_name}' truncated.")
-        #cursor_obj.execute(truncate_query)
     else:
         # Create table if table doesn't exist
-        create_table_query = f"CREATE TABLE {table_name}(id serial NOT NULL PRIMARY KEY,F_name varchar(255), T_name varchar, start_date date, expiry_date date,File_schema json,Table_schema json,File_table_mapping json,Process_file boolean,active_flag boolean, update_type VARCHAR(255), track_changes varchar(225))"
+        create_table_query = f"CREATE TABLE {table_name}(id serial NOT NULL PRIMARY KEY,F_name varchar(255), T_name varchar, start_date date, expiry_date date,File_schema json,Table_schema json,File_table_mapping json,Process_flag boolean,active_flag boolean, update_type VARCHAR(255), track_changes varchar(225))"
         cursor_obj.execute(create_table_query)
         print(f"The {table_name} is created")
 
-# def get_csv_file_names(folder_path):
-#     file_names = os.listdir(folder_path)
-#     file_names_list=[]
-#     for file_name in file_names:
-#         if file_name.endswith(".csv"):
-#             file_names_list.append(file_name)
-#     return file_names_list
-
-# def insert_data(file_names_list,folder_path):
-#     for file in file_names_list:
-#         file_path=folder_path+"\\"+file
-#         # print("file",file_path)
-#         df = pd.read_csv(file_path)
-#         metadata = df.dtypes.apply(lambda x: x.name).to_dict()
-#         metadata_json = []
-#         ft_map = []
-#         for k, v in metadata.items():
-#             file_schema = {
-#                 "column_name": k, "data_type": v, "primary_key": False, "is_unique": False, "is_null": False
-#             }
-#             metadata_json.append(file_schema)
-#             mdata = json.dumps(metadata_json, indent=4)
-#             ft_json = {
-#             "f_column": k , "t_column": k
-#             }
-#             ft_map.append(ft_json)
-#             #print(".........",ft_map)
-
-#         #Remove extension from file name
-#         li=file.split(".")
-#         current_date = date.today()
-#         # Insert values into the configure_Table
-#         insert_query = f"INSERT INTO cardworks_internal.public.{table_name}(F_name, T_name, start_date, File_schema, Table_schema, File_table_mapping, Process_file,active_flag,update_type,track_changes) VALUES (%s, %s, %s, %s, %s, %s, %s,%s,%s,%s)"
-#         cursor.execute(insert_query, (li[0], li[0],current_date,mdata, mdata, json.dumps(ft_map,indent=4), True, True,"Manual","changes"))   
-#     print("Data inserted")
 
 con=pg_connect(postgresql_database,postgresql_user,postgresql_password,postgresql_host,postgresql_port)
 print("Postgresql connection established successfully")
 table_name='config_table'
 cursor = con.cursor()
 create_table(table_name,cursor)
-#folder_path = os.getcwd()
-# # folder_path = 'C:\\Users\\rajkumar.medishetti\\Desktop\\Card works\\test_files'
-# file_names_list=get_csv_file_names(folder_path)
-# print("File names list ",file_names_list)
-# insert_data(file_names_list,folder_path)
 con.commit()
 
 
--- mdata_compare.py
+++ mdata_compare.py
@@ -27,72 +27,9 @@ def pg_connect(database,user,password,host,port):
     )
     return connection
 con=pg_connect(postgresql_database,postgresql_user,postgresql_password,postgresql_host,postgresql_port)
-#print("Postgresql connection established successfully")
 
 
-table_name='config_table'
-cursor = con.cursor()
-
-"""folder_path = 'C:/Users/sumanth.gorantla/Desktop/sumanth_data/sample datasets'
-# Get a list of all files in the specified folder
-file_names = os.listdir(folder_path)
-file_names_list=[]
-# Print the file names
-for file_name in file_names:
-    file_names_list.append(file_name)
-#print("...",file_names_list)
-
-for file in file_names_list:
-    folder_path = 'C:/Users/sumanth.gorantla/Desktop/sumanth_data/sample datasets'
-    folder_path=folder_path+"\\"+file
-    #print("file",folder_path)
-    df = pd.read_csv(folder_path)
-    metadata = df.dtypes.apply(lambda x: x.name).to_dict()
-    metadata_json = []
-    ft_map = []
-    for k, v in metadata.items():
-        file_schema = {
-            "column_name": k, "data_type": v, "primary_key": False, "is_unique": False, "is_null": False
-        }
-        metadata_json.append(file_schema)
-        mdata = json.dumps(metadata_json)
-        #print(mdata)
-        ft_json = {
-            "f_column": k , "t_column": k
-        }
-        ft_map.append(ft_json)
-        #print(".........",ft_map)
-    #print(type(metadata_json))
-    #Remove extension from file name
-    fi=Path(file).stem
-    #print(fi)
-    current_date = date.today()
-    # Insert values into the configure_Table   
-
-
-    insert_query = f"INSERT INTO cardworks_internal.public.{table_name}(F_name, T_name, start_date,File_schema, Table_schema, File_table_mapping, Process_file,active_flag,update_type,track_changes) VALUES (%s, %s, %s, %s, %s, %s, %s,%s,%s,%s)"
-    cursor.execute(f"SELECT EXISTS(SELECT 1 FROM {table_name} where f_name = %s);", (fi,))
-    fi_exists = cursor.fetchone()[0]
-    #print(fi_exists)
-    if fi_exists:
-        print(f"the file with the name {fi} already exists.....comparing metadata of both files")
-        query=f"select file_schema from {table_name} where f_name like %s "
-        cursor.execute(query,(fi+'%',))
-        existing_file_schema = cursor.fetchall()
-        #print(existing_file_schema[0][0])
-        diff = DeepDiff(existing_file_schema[0][0], metadata_json, ignore_order=True)
-        print(diff)
-        if diff=={}:
-            print("No changes in metadata")
-        else:
-            cursor.execute(insert_query, (fi, fi,current_date,mdata, mdata, json.dumps(ft_map,indent=4), True, True,"Manual","changes"))
-    else:
-        cursor.execute(insert_query, (fi, fi,current_date,mdata, mdata, json.dumps(ft_map,indent=4), True, True,"Manual","changes"))
-
-
-con.commit()"""
-
-
+#returns files list in local folder
 def get_csv_file_names(folder_path):
     file_names = os.listdir(folder_path)
     file_names_list=[]
@@ -101,6 +38,7 @@ def get_csv_file_names(folder_path):
             file_names_list.append(file_name)
     return file_names_list
 
+#inserts row in config_table when new file is detected
 def insert_data(file_names_list,folder_path):
     for file in file_names_list:
         file_path=folder_path+"\\"+file
@@ -110,53 +48,42 @@ def insert_data(file_names_list,folder_path):
         metadata_json = []
         ft_map = []
         for k, v in metadata.items():
-            file_schema = {
-                "column_name": k, "data_type": v, "primary_key": False, "is_unique": False, "is_null": False
-            }
+            file_schema = {"column_name": k, "data_type": v, "primary_key": False, "is_unique": False, "is_null": False}
             metadata_json.append(file_schema)
-            mdata = json.dumps(metadata_json, indent=4)
-            ft_json = {
-            "f_column": k , "t_column": k
-            }
+            ft_json = {"f_column": k , "t_column": k}
             ft_map.append(ft_json)
-            #print(".........",ft_map)
 
-        #print(len(metadata_json[0]))
         for k in range(0,len(metadata_json)):
             column_name=metadata_json[k]["column_name"]
-            #print(column_name)
             dtype=metadata_json[k]["data_type"]
-            #print(dtype)
             if dtype=="object":
                 metadata_json[k]["data_type"]="VARCHAR(255)"
-                #dtype="VARCHAR(255)"
             elif dtype=="int64":
                 metadata_json[k]["data_type"]="bigint"
-                #dtype="bigint"
             elif dtype=="float64":
                 metadata_json[k]["data_type"]="float"
-                #dtype="float"
+        mdata = json.dumps(metadata_json, indent=4)
         print(metadata_json)
         #Remove extension from file name
         fi=Path(file).stem
         current_date = date.today()
         # Insert values into the configure_Table
-        insert_query = f"INSERT INTO cardworks_internal.public.{table_name}(F_name, T_name, start_date, File_schema, Table_schema, File_table_mapping, Process_file,active_flag,update_type,track_changes) VALUES (%s, %s, %s, %s, %s, %s, %s,%s,%s,%s)"
+        insert_query = f"INSERT INTO cardworks_internal.public.{table_name}(F_name, T_name, start_date,File_schema, Table_schema, File_table_mapping, Process_flag, active_flag, update_type,track_changes) VALUES (%s, %s, %s, %s, %s,%s,%s,%s,%s,%s)"
+        # update_query = f"UPDATE cardworks_internal.public.{table_name} SET expiry_date = {} WHERE f_name = {fi}"
         cursor.execute(f"SELECT EXISTS(SELECT 1 FROM {table_name} where f_name = %s);", (fi,))
         fi_exists = cursor.fetchone()[0]
-        #print(fi_exists)
         if fi_exists:
             print(f"the file with the name {fi} already exists.....comparing metadata of both files")
             query=f"select file_schema from {table_name} where f_name like %s "
             cursor.execute(query,(fi+'%',))
             existing_file_schema = cursor.fetchall()
-            #print(existing_file_schema[0][0])
             diff = DeepDiff(existing_file_schema[0][0], metadata_json, ignore_order=True)
-            #print(diff)
+            dif=json.dumps(diff)
             if diff=={}:
                 print("No changes in metadata")
+                cursor.execute(insert_query, (fi, fi,current_date,mdata, mdata, json.dumps(ft_map,indent=4), True, True,"Manual","No changes"))
             else:
-                cursor.execute(insert_query, (fi, fi,current_date,mdata, mdata, json.dumps(ft_map,indent=4), True, True,"Manual",diff))
+                cursor.execute(insert_query, (fi, fi,current_date, mdata, mdata, json.dumps(ft_map,indent=4), True, True,"Manual",dif))
         else:
             cursor.execute(insert_query, (fi, fi,current_date,mdata, mdata, json.dumps(ft_map,indent=4), True, True,"Manual","changes"))
 
@@ -164,8 +91,8 @@ def insert_data(file_names_list,folder_path):
     print("Data inserted")
 
 folder_path = os.getcwd()
- # folder_path = 'C:\\Users\\rajkumar.medishetti\\Desktop\\Card works\\test_files'
-file_names_list=get_csv_file_names(folder_path)
-#print("File names list ",file_names_list)
+file_names_list = get_csv_file_names(folder_path)
+table_name ='config_table'
+cursor = con.cursor()
 insert_data(file_names_list,folder_path)
 con.commit()
